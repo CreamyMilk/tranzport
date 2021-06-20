4e88cdcf84ee2032d19c0a9c18d318714e83aec0
CreamyMilk <creamym254@gmail.com>
2021-06-20 12:06:59 +0300

    🦀 Minimal Example

--- src/main.rs
+++ src/main.rs
@@ -1,84 +1,119 @@
-use serde::{Deserialize, Serialize};
-use std::collections::HashMap;
-use std::io;
-
 use actix_web::{
-    client::Client,
-    error::ErrorBadRequest,
-    web::{self, BytesMut},
-    App, Error, HttpResponse, HttpServer,
+    error, middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer,
 };
 use futures::StreamExt;
-use validator::Validate;
-use validator_derive::Validate;
+use json::JsonValue;
+use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Validate, Deserialize, Serialize)]
-struct UserReq {
-    #[validate(length(min = 1, max = 1000000))]
-    id: String,
-    #[validate(length(min = 1, max = 100))]
+#[derive(Debug, Serialize, Deserialize)]
+struct MyObj {
     name: String,
+    number: i32,
 }
 
-
-#[derive(Debug, Deserialize)]
-struct HttpBinResponse {
-    args: HashMap<String, String>,
-    data: String,
-    files: HashMap<String, String>,
-    form: HashMap<String, String>,
-    headers: HashMap<String, String>,
-    json: UserReq,
-    origin: String,
-    url: String,
+/// This handler uses json extractor
+async fn index(item: web::Json<MyObj>) -> HttpResponse {
+    println!("Automajically extacted model: {:?}", &item);
+    HttpResponse::Ok().json(item.0) // <- send response
 }
 
-/// validate data, post json to httpbin, get it back in the response body, return deserialized
-async fn step_x(data: UserReq, client: &Client) -> Result<UserReq, Error> {
-    // validate data
-    data.validate().map_err(ErrorBadRequest)?;
+/// This handler uses json extractor with limit
+async fn extract_item(item: web::Json<MyObj>, req: HttpRequest) -> HttpResponse {
+    println!("request: {:?}", req);
+    println!("model: {:?}", item);
 
-    let mut res = client
-        .post("http://httpbin.org/post")
-        .send_json(&data)
-        .await
-        .map_err(Error::from)?; // <- convert SendRequestError to an Error
+    HttpResponse::Ok().json(item.0) // <- send json response
+}
 
-    let mut body = BytesMut::new();
-    while let Some(chunk) = res.next().await {
-        body.extend_from_slice(&chunk?);
+const MAX_SIZE: usize = 262_144; // max payload size is 256k
+
+/// This handler manually load request payload and parse json object
+async fn index_manual(mut payload: web::Payload) -> Result<HttpResponse, Error> {
+    // payload is a stream of Bytes objects
+    let mut body = web::BytesMut::new();
+    while let Some(chunk) = payload.next().await {
+        let chunk = chunk?;
+        // limit max size of in-memory payload
+        if (body.len() + chunk.len()) > MAX_SIZE {
+            return Err(error::ErrorBadRequest("overflow"));
+        }
+        body.extend_from_slice(&chunk);
     }
 
-    let body: HttpBinResponse = serde_json::from_slice(&body).unwrap();
-    Ok(body.json)
+    // body is loaded, now we can deserialize serde-json
+    let obj = serde_json::from_slice::<MyObj>(&body)?;
+    Ok(HttpResponse::Ok().json(obj)) // <- send response
 }
 
-async fn create_something(
-    some_data: web::Json<UserReq>,
-    client: web::Data<Client>,
-) -> Result<HttpResponse, Error> {
-    let some_data_2 = step_x(some_data.into_inner(), &client).await?;
-    let some_data_3 = step_x(some_data_2, &client).await?;
-    let d = step_x(some_data_3, &client).await?;
-
+/// This handler manually load request payload and parse json-rust
+async fn index_mjsonrust(body: web::Bytes) -> Result<HttpResponse, Error> {
+    // body is loaded, now we can deserialize json-rust
+    let result = json::parse(std::str::from_utf8(&body).unwrap()); // return Result
+    let injson: JsonValue = match result {
+        Ok(v) => v,
+        Err(e) => json::object! {"err" => e.to_string() },
+    };
     Ok(HttpResponse::Ok()
         .content_type("application/json")
-        .body(serde_json::to_string(&d).unwrap()))
+        .body(injson.dump()))
 }
 
 #[actix_web::main]
-async fn main() -> io::Result<()> {
+async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
-    let endpoint = "127.0.0.1:8080";
 
-    println!("Starting server at: {:?}", endpoint);
     HttpServer::new(|| {
         App::new()
-            .data(Client::default())
-            .service(web::resource("/something").route(web::post().to(create_something)))
+            // enable logger
+            .wrap(middleware::Logger::default())
+            .data(web::JsonConfig::default().limit(4096)) // <- limit size of the payload (global configuration)
+            .service(web::resource("/extractor").route(web::post().to(index)))
+            .service(
+                web::resource("/extractor2")
+                    .data(web::JsonConfig::default().limit(1024)) // <- limit size of the payload (resource level)
+                    .route(web::post().to(extract_item)),
+            )
+            .service(web::resource("/manual").route(web::post().to(index_manual)))
+            .service(web::resource("/mjsonrust").route(web::post().to(index_mjsonrust)))
+            .service(web::resource("/").route(web::post().to(index)))
     })
-    .bind(endpoint)?
+    .bind("127.0.0.1:8080")?
     .run()
     .await
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::dev::Service;
+    use actix_web::{http, test, web, App};
+
+    #[actix_rt::test]
+    async fn test_index() -> Result<(), Error> {
+        let mut app = test::init_service(
+            App::new().service(web::resource("/").route(web::post().to(index))),
+        )
+        .await;
+
+        let req = test::TestRequest::post()
+            .uri("/")
+            .set_json(&MyObj {
+                name: "my-name".to_owned(),
+                number: 43,
+            })
+            .to_request();
+        let resp = app.call(req).await.unwrap();
+
+        assert_eq!(resp.status(), http::StatusCode::OK);
+
+        let response_body = match resp.response().body().as_ref() {
+            Some(actix_web::body::Body::Bytes(bytes)) => bytes,
+            _ => panic!("Response error"),
+        };
+
+        assert_eq!(response_body, r##"{"name":"my-name","number":43}"##);
+
+        Ok(())
+    }
 }
