0b8ee4f97cf61e459ae4b9a5bf55eca880dc053d
CreamyMilk <creamym254@gmail.com>
2021-06-22 19:24:19 +0300

    🦀 Added methods to support messageDumps

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,65 @@
-use actix_web::{
-     middleware, web, App, HttpRequest, HttpResponse, HttpServer,
-};
+use actix_web::{middleware, web, App, HttpRequest, HttpResponse, HttpServer};
 use serde::{Deserialize, Serialize};
-
+mod reconst;
 
 #[derive(Debug, Serialize, Deserialize)]
-struct MyObj {
-    name: String,
-    number: i32,
+pub struct MyObj {
+    message: String,
 }
 
-/// This handler uses json extractor
-async fn index(item: web::Json<MyObj>,req :HttpRequest) -> HttpResponse {
-    println!("Automajically extacted model: {:?}", &item);
-    println!("request: {:?}", req.headers());
-
-    HttpResponse::Ok().json(item.0) // <- send response
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct ResponseStruct {
+    pub message_dump: Vec<MessageDump>,
 }
 
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct MessageDump {
+    pub subscriptionid: String,
+    pub message_timestamp: String,
+    pub thread: String,
+    pub phone_number: String,
+    #[serde(rename = "MessageSubject")]
+    pub message_subject: String,
+    pub messagetxt: String,
+}
 
-/// This handler uses json extractor
-async fn index2(item: web::Json<MyObj>,req :HttpRequest ) -> HttpResponse {
-    let m = MyObj{name:"naming".to_string(),number:100};
-    let pp = [m];
-    println!("request: {:?}", req);
-    println!("model: {:?}", item);
-    HttpResponse::Ok().json(pp)
+impl MessageDump {
+    fn classify(&self) {
+        println!("Buda hii ni {}", self.message_timestamp);
+    }
+}
+
+async fn message_handler(m: web::Json<ResponseStruct>, _req: HttpRequest) -> HttpResponse {
+    for x in m.message_dump.iter() {
+        x.classify();
+    }
+    let res = MyObj{message:"Guess Thanks".to_string()};
+    HttpResponse::Ok().json(res)
+}
+
+async fn hello_handler(_req: HttpRequest) -> HttpResponse {
+    let m = MyObj {
+        message: "We Runninng my broder".to_string(),
+    };
+    HttpResponse::Ok().json(m)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    println!("{}", reconst::ISDEPOSIT);
     std::env::set_var("RUST_LOG", "actix_web=info");
     env_logger::init();
 
     HttpServer::new(|| {
         App::new()
-            // enable logger
             .wrap(middleware::Logger::default())
-            .data(web::JsonConfig::default().limit(4096)) // <- limit size of the payload (global configuration)
-            .service(web::resource("/").route(web::post().to(index)))
-            .service(web::resource("/a").route(web::post().to(index2)))
+            .data(web::JsonConfig::default().limit(4096000))
+            .service(web::resource("/").route(web::get().to(hello_handler)))
+            .service(web::resource("/data").route(web::post().to(message_handler)))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("0.0.0.0:9000")?
     .run()
     .await
 }
